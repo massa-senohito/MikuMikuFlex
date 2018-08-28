a7e64c55abb1594418b5a27990e2414eb5f17800
ＦＲＯＭ <kumakamikoubou@gmail.com>
2018-08-28 20:26:40 +0900

    nuspec のバージョン変更忘れ。

--- make_nupkg.bat
+++ make_nupkg.bat
@@ -1,7 +1,9 @@
 echo off
 echo ------------------------------------------------------------
 echo MikuMikuFlex の NuGet パッケージ (.nupkg) 作成バッチ
-echo 事前に、x86/Release と x64/Release のビルドを完了しておくこと。
+echo 事前に x86/Release  のビルドを完了しておくこと。(x64もビルドできるが、NuGetにはx86のみ公開する。）
+echo また、NuGetパッケージの属性（バージョンなど）が変わったら、
+echo MikuMikuFlex/MikuMikuFlex.nuspec を修正すること。
 echo ------------------------------------------------------------
 
 nuget pack MikuMikuFlex\MikuMikuFlex.csproj -IncludeReferencedProjects -properties Configuration=Release;Platform=x86 -OutputDirectory nuget_packages
