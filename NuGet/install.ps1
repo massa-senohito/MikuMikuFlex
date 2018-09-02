9876cef7192da010e671f3713c0d72f68552666f
ＦＲＯＭ <kumakamikoubou@gmail.com>
2018-09-03 00:52:31 +0900

    NuGetからインストールした場合に、ビルドはできても、ファイル不足で実行時に例外が発生する不具合を修正。
    install.ps1 を追加し、NuGet でのインストール時には toon*.bmp と sharpdx_direct3d11_1_effects_*.dll を「ビルドアクション：なし」「新しい場合は常にコピーする」ようにプロパティを変更するようにした。
    nuspec への TgaLib.dll の記載忘れ。
    TargaImage.dll が不要だったので削除。

---
+++ NuGet/install.ps1
@@ -0,0 +1,23 @@
+﻿# NuGet でのインストール時、toon0～10.bmp のプロパティを変更する。
+# 参考: http://garicchi.hatenablog.jp/entry/2015/06/01/185201
+
+param($installPath, $toolsPath, $package, $project)
+
+function Recurse($dir)
+{
+    foreach($i in $dir)
+    {
+        Recurse($i.ProjectItems)
+
+        if(($i.Name -like "toon*.bmp") -Or ($i.Name -like "sharpdx_direct3d11_1_effects_*.dll"))
+        {
+			# 0:None, 1:Compile, 2:Content, 3:EmbeddedResource
+            $i.Properties.Item("BuildAction").Value = [int]0
+			
+			# 0:DontCopy, 1:CopyAlways, 2:CopyIfNewer
+            $i.Properties.Item("CopyToOutputDirectory").Value = [int]2
+        }
+    }
+}
+
+Recurse($project.ProjectItems);
