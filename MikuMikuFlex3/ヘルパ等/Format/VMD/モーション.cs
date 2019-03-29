d9dd23209e79ec1ee1091ef5b83258789e398b02
ＦＲＯＭ <kumakamikoubou@gmail.com>
2019-03-29 15:34:31 +0900

    VMD/モーション.cs が抜けていたので追加。

---
+++ MikuMikuFlex3/ヘルパ等/Format/VMD/モーション.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace MikuMikuFlex3.VMDFormat
+{
+    public class モーション
+    {
+        public ヘッダ ヘッダ;
+
+        public ボーンフレームリスト ボーンフレームリスト;
+
+        public モーフフレームリスト モーフフレームリスト;
+
+        public カメラフレームリスト カメラフレームリスト;
+
+        public 照明フレームリスト 照明フレームリスト;
+
+        // todo: VMDのセルフ影への対応
+        //public セルフ影リスト セルフ影リスト;
+
+        // todo: VMDのモデル表示・IK on/off への対応
+        //public モデル表示_IKリスト モデル表示_IKリスト;
+
+
+        public モーション()
+        {
+        }
+
+        /// <summary>
+        ///     指定されたストリームから読み込む。
+        /// </summary>
+        public モーション( Stream fs )
+        {
+            this.ヘッダ = new ヘッダ( fs );
+            this.ボーンフレームリスト = new ボーンフレームリスト( fs );
+            this.モーフフレームリスト = new モーフフレームリスト( fs );
+            this.カメラフレームリスト = new カメラフレームリスト( fs );
+            this.照明フレームリスト = new 照明フレームリスト( fs );
+            //this.セルフ影リスト = new セルフ影リスト( fs );
+            //this.モデル表示_IKリスト = new モデル表示_IKリスト( fs );
+        }
+    }
+}
