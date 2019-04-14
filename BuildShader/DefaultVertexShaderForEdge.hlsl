efb4e2c4bb03a4861d16a7a6e5ca32810ee58d6f
ＦＲＯＭ <kumakamikoubou@gmail.com>
2019-04-15 00:19:26 +0900

    エッジを微修正。

--- BuildShader/DefaultVertexShaderForEdge.hlsl
+++ BuildShader/DefaultVertexShaderForEdge.hlsl
@@ -17,8 +17,8 @@ VS_OUTPUT main(VS_INPUT input)
 
 	// 位置
     float4 position = input.Position;
-    // 法線方向に膨らませる。
-    position = input.Position + float4(Out.Normal, 0) * g_EdgeWidth * input.EdgeWeight * distance(input.Position.xyz, g_CameraPosition.xyz) * 0.0010;
+	// 法線方向に膨らませる。
+	position += float4(Out.Normal, 1) * g_EdgeWidth * input.EdgeWeight * distance(input.Position, g_CameraPosition) * 0.0010;
     Out.Position = mul(position, g_WorldMatrix); // ワールド変換
 
 	// カメラとの相対位置
